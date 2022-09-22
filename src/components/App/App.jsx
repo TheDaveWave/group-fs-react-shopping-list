7cde5bb36b3e734621f7b07252fba4be9b65c24a
David Lindberg <david.wesley.lindberg@gmail.com>
2022-09-22 13:42:37 -0500

    added partial get request axios

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import axios from 'axios';
+import {useState, useEffect} from 'react';
 import Header from '../Header/Header.jsx'
 import './App.css';
 
 
 function App() {
+    const [productList, setProductList] = useState([]);
+
+    const getProducts = () => {
+        axios.get('/products')
+        .then(response => {
+            setProductList(response.data);
+        })
+    }
+
     return (
         <div className="App">
             <Header />
