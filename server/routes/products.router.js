3cb02705526d95319eac9c2eda2fb1d62ee94de5
David Lindberg <david.wesley.lindberg@gmail.com>
2022-09-22 14:38:21 -0500

    created successful DELETE route the truncates the table

--- server/routes/products.router.js
+++ server/routes/products.router.js
@@ -18,7 +18,20 @@ router.get('/', (req,res) => {
 // POST route /products
 
 // DELETE route /products/clear
-
+router.delete('/clear', (req,res) => {
+    console.log('In router DELETE /products/clear');
+    // I am gonna delete all the data in your database >:)
+    const queryText = `TRUNCATE TABLE "products";`;
+    pool.query(queryText)
+    .then(()=> {
+        console.log('All products deleted!');
+        res.sendStatus(201);
+    })
+    .catch(err => {
+        console.log('Error in deleting rows', err)
+        res.sendStatus(500);
+    })
+});
 // DELETE route /products/remove/:id
 
 // PUT route /products/reset
