c93112a96ec5d75b77503cfb3377221f46826d98
David Lindberg <david.wesley.lindberg@gmail.com>
2022-09-22 11:53:57 -0500

    added SQL table and test values into database.sql

--- database.sql
+++ database.sql
@@ -1,2 +1,13 @@
 -- Don't forget to add your create table SQL 
 -- It is also helpful to include some test data
+
+CREATE TABLE "products" (
+	"id" SERIAL PRIMARY KEY,
+	"name" VARCHAR(25) NOT NULL,
+	"quantity" INTEGER NOT NULL,
+	"unit" VARCHAR(15) NOT NULL,
+	"isPurchased" BOOLEAN DEFAULT 'false'
+);
+
+INSERT INTO "products" ("name", "quantity", "unit")
+VALUES ('apples', '2', 'lbs'), ('bread', '1', 'loaf'), ('milk', '3', 'gallon');
