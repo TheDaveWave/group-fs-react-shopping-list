a61e1e797c8de210f7740135f2b95d7bd7eb7c4a
David Lindberg <david.wesley.lindberg@gmail.com>
2022-09-22 12:58:18 -0500

    added comments to router.js

--- server/routes/products.router.js
+++ server/routes/products.router.js
@@ -1,6 +1,17 @@
 const router = require('express').Router();
 const pool = require('../modules/pool');
 
-// put routes here
+// GET route /products
+
+// POST route /products
+
+// DELETE route /products/clear
+
+// DELETE route /products/remove/:id
+
+// PUT route /products/reset
+
+// PUT route /products/purchase/:id
+
 
 module.exports = router;
