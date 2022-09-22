f1c92bb1681fa90058aa9a7d34ec4ae1e8b26ee0
David Lindberg <david.wesley.lindberg@gmail.com>
2022-09-22 12:44:01 -0500

    set up boiler code in server.js

--- server/server.js
+++ server/server.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5000;
 
 /** ---------- MIDDLEWARE ---------- **/
-app.use(bodyParser.json()); // needed for axios requests
+app.use(express.json()); // needed for axios requests
 app.use(express.static('build'));
 
 /** ---------- EXPRESS ROUTES ---------- **/
 // Create your API routes in a separate file
 // and plug them in here with `app.use()`
+const productRouter = require('./routes/products.router');
+app.use('/products', productRouter);
 
 /** ---------- START SERVER ---------- **/
 app.listen(PORT,  () => {
