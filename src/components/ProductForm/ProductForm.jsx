84e48f5affab085caa242ae3b7506379956cca18
Joey Pettit <pettitbass@gmail.com>
2022-09-22 15:40:34 -0500

    finished basic product form, not tested yet

--- src/components/ProductForm/ProductForm.jsx
+++ src/components/ProductForm/ProductForm.jsx
@@ -1,10 +1,46 @@
 import { useState, useEffect } from "react";
+import axios from "axios";
 
-function ProductForm () {
+function ProductForm ({getProducts}) {
+    const [name, setName] = useState('');
+    const [quantity, setQuantity] = useState('');
+    const [unit, setUnit] = useState('');
+
+    function addProduct(){
+        axios({
+            method: 'Post',
+            url: '/products/',
+            data: {
+                name: name,
+                quantity: quantity,
+                unit: unit
+            }
+        }).then(() => {
+            console.log('POST new product succcessful!')
+            // fetch updated list
+            getProducts();
+
+            // clear form
+            setName('');
+            setQuantity('');
+            setUnit('');
+        }).catch(err => alert(err));
 
 
+    }
 
 
+    return(
+        <>
+            <form>
+                <input value={name} onChange = {event => (event.target.value)}></input>
+                <input value={quantity} onChange ={ event => (event.target.value)}></input>
+                <input value={unit} onChange={event => (event.target.value)}></input>
+                <button onClick={addProduct}>Add Product</button>
+            </form>
+
+        </>
+    )
 
 }
 
